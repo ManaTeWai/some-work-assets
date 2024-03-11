cc8475d442534b3cb55fdb51fa5860f711791e94
ManaTeWai <maksimts03@gmail.com>
2024-03-11 09:41:51 +0300

    mobile_test

--- script/script_v1.js
+++ script/script_v1.js
@@ -20,4 +20,32 @@ let scrollToTop = function () {
 }
 
 window.addEventListener("scroll", myScrollFunc);
-scrollBtn.addEventListener("click", scrollToTop)
+scrollBtn.addEventListener("click", scrollToTop);
+
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menu_inner");
+const humburger = document.querySelector(".humburger");
+const closeIcon = document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+	if (menu.classList.contains("showMenu")) {
+		menu.classList.remove("showMenu");
+		closeIcon.style.display = "none";
+		menuIcon.style.display = "block";
+		document.body.style.overflow = 'auto';
+	} else {
+		menu.classList.add("showMenu");
+		closeIcon.style.display = "block";
+		menuIcon.style.display = "none";
+		document.body.style.overflow = 'hidden';
+	}
+}
+
+menuItems.forEach(
+	function (menuItem) {
+		menuItem.addEventListener("click", toggleMenu);
+	}
+)
+
+humburger.addEventListener("click", toggleMenu);
