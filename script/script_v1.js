f9c4a84bd50d796a788772ce491715ec1607cdd8
ManaTeWai <maksimts03@gmail.com>
2024-03-12 16:09:19 +0300

    03_12

--- script/script_v1.js
+++ script/script_v1.js
@@ -31,21 +31,15 @@ const menuIcon = document.querySelector(".menuIcon");
 function toggleMenu() {
 	if (menu.classList.contains("showMenu")) {
 		menu.classList.remove("showMenu");
+		menu.style.display = "none"
 		closeIcon.style.display = "none";
 		menuIcon.style.display = "block";
-		document.body.style.overflow = 'auto';
 	} else {
 		menu.classList.add("showMenu");
+		menu.style.display = "block"
 		closeIcon.style.display = "block";
 		menuIcon.style.display = "none";
-		document.body.style.overflow = 'hidden';
 	}
 }
-
-menuItems.forEach(
-	function (menuItem) {
-		menuItem.addEventListener("click", toggleMenu);
-	}
-)
 
 humburger.addEventListener("click", toggleMenu);
